bbab711726700823198e9d57119cd216496a657d
tsts9701 <168278649+tsts9701@users.noreply.github.com>
2025-04-22 17:16:58 +0300

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -4,16 +4,16 @@ export default function Document() {
   return (
     <Html lang="ru">
       <Head>
-        <meta name="description" content="Оригинальная спортивная обувь из Европы: кроссовки, кеды, зимняя обувь, ботинки." />
+        <meta name="description" content="Якісний кратом по найкращим цінам" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="кроссовки, спортивная обувь, кроссовки nike, кроссовки adidas, кроссовки jordan, купить nike, купить jordan, обувь из Европы, кроссовки из Европы" />
-        <meta name="og:title" content="Cross Centre — интернет-магазин спортивной обуви из Европы" />
+        <meta name="og:title" content="Kratom-in.com.ua — інтернет магазин колекційного чаю" />
         <meta name="og:type" content="website" />
         <meta name="og:url" content="https://tea-store-tau.vercel.app" />
         <meta property="og:site_name" content="Cross Centre"></meta>
         <meta name="og:image" content="https://tea-store-tau.vercel.app/logotype2.png" />
         <meta property="og:image:secure_url" content="https://tea-store-tau.vercel.app/logotype2.png" /> 
-        <meta property="og:image:type" content="image/jpeg" /> 
+        <meta property="og:image:type" content="image/png" /> 
         <meta property="og:image:width" content="1200" /> 
         <meta property="og:image:height" content="630" />
       </Head>
