4cdd351d8e8f642f745d622a14a5300bc22d4a71
tsts9701 <168278649+tsts9701@users.noreply.github.com>
2025-04-23 22:12:27 +0300

    cm

--- pages/_app.js
+++ pages/_app.js
@@ -42,7 +42,7 @@ function App({ Component, pageProps }) {
         <meta name="keywords" content="кратом, kratom, green maeng, white maeng, зелений кратом, заменитель кратома, замінник кратома, матча" />
         <link rel="icon" href="/favicon.webp" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <script async referrerPolicy="no-referrer" src="https://script.click-chat.ru/chat.js?wid=06c53cd7-fc2c-4a15-8fbe-f133212deafd"></script>
+        <script async src="/clickChat.js"></script>
         <script async src="https://www.w3counter.com/tracker.js?id=152206"></script>
         <link
           rel="preconnect"
