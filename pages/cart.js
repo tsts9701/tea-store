93720a64166846ea5e9acf3bd21b93c32ef8e3fb
tsts9701 <168278649+tsts9701@users.noreply.github.com>
2025-05-01 12:20:29 +0300

    cm

--- pages/cart.js
+++ pages/cart.js
@@ -175,7 +175,7 @@ const Cart = () => {
                             Замовте наші товари, вам сподобається :)
                         </span>
                         <Link
-                            href="/"
+                            href="/category/all-products"
                             className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
                         >
                             За покупками
